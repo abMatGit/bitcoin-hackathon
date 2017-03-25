4a93d5994e3b364ec54845ce95ca8265578a2892
luis campos <lcamposguajardo@gmail.com>
2017-03-25 16:18:45 -0700

    basics for queue on node

---
+++ app/server.js
@@ -0,0 +1,29 @@
+'use strict'
+
+var kue = require('kue'),
+  jobs = kue.createQueue();
+
+function newJob (name){
+  name = name || 'Default_Name';
+  var job = jobs.create('new job', {
+    name: name
+  });
+
+  job.on('complete', function (){
+      console.log('Job', job.id, 'with name', job.data.name, 'is done');
+    }).on('failed', function (){
+      console.log('Job', job.id, 'with name', job.data.name, 'has failed');
+    })
+
+  job.save();
+}
+
+jobs.process('new job', function (job, done){
+  /* carry out all the job function here */
+  done && done();
+});
+
+
+setInterval(function (){
+  newJob('Send_Email');
+}, 3000);
